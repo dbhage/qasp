310d2727e60837ecb677542576491270d7b464f7
Dwijesh Bhageerutty <neerav789@gmail.com>
2013-12-08 17:12:13 -0500

    [Database commands] SQL statements which were used to populate primes table

---
+++ populatePrimesTable.sql
@@ -0,0 +1,87 @@
+INSERT INTO primes VALUES ('I', '&Substantives', '1');
+INSERT INTO primes VALUES ('YOU', '&Substantives', '1');
+INSERT INTO primes VALUES ('SOMETHING', '&Substantives', '1');
+INSERT INTO primes VALUES ('THING', '&Substantives', '1');
+INSERT INTO primes VALUES ('PEOPLE', '&Substantives', '1');
+INSERT INTO primes VALUES ('BODY', '&Substantives', '1');
+
+INSERT INTO primes VALUES ('KIND', '&RelSubstantives', '1');
+INSERT INTO primes VALUES ('PART', '&RelSubstantives', '2');
+
+INSERT INTO primes VALUES ('ONE', '&Quantifiers', '1');
+INSERT INTO primes VALUES ('TWO', '&Quantifiers', '1');
+INSERT INTO primes VALUES ('MUCH', '&Quantifiers', '1');
+INSERT INTO primes VALUES ('MANY', '&Quantifiers', '1');
+INSERT INTO primes VALUES ('LITTLE', '&Quantifiers', '1');
+INSERT INTO primes VALUES ('FEW', '&Quantifiers', '1');
+INSERT INTO primes VALUES ('SOME', '&Quantifiers', '1');
+INSERT INTO primes VALUES ('ALL', '&Quantifiers', '1');
+
+INSERT INTO primes VALUES ('THIS', '&Determiners', '1');
+INSERT INTO primes VALUES ('THE', '&Determiners', '2');
+INSERT INTO primes VALUES ('SAME', '&Determiners', '1');
+INSERT INTO primes VALUES ('OTHER', '&Determiners', '2');
+INSERT INTO primes VALUES ('ELSE', '&Determiners', '1');
+
+INSERT INTO primes VALUES ('GOOD', '&Evaluators', '1');
+INSERT INTO primes VALUES ('BAD', '&Evaluators', '1');
+
+INSERT INTO primes VALUES ('BIG', '&Descriptors', '1');
+INSERT INTO primes VALUES ('SMALL', '&Descriptors', '1');
+
+INSERT INTO primes VALUES ('KNOW', '&MentalPredicates', '1');
+INSERT INTO primes VALUES ('THINK', '&MentalPredicates', '1');
+INSERT INTO primes VALUES ('WANT', '&MentalPredicates', '1');
+INSERT INTO primes VALUES ('FEEL', '&MentalPredicates', '1');
+INSERT INTO primes VALUES ('SEE', '&MentalPredicates', '1');
+INSERT INTO primes VALUES ('HEAR', '&MentalPredicates', '1');
+
+INSERT INTO primes VALUES ('SAY', '&Speech', '1');
+INSERT INTO primes VALUES ('WORD', '&Speech', '1');
+INSERT INTO primes VALUES ('TRUE', '&Speech', '1');
+
+INSERT INTO primes VALUES ('DO', '&Aemc', '2');
+INSERT INTO primes VALUES ('HAPPEN', '&Aemc', '1');
+INSERT INTO primes VALUES ('MOVE', '&Aemc', '2');
+INSERT INTO primes VALUES ('TOUCH', '&Aemc', '2');
+
+INSERT INTO primes VALUES ('BE', '&Leps', '2');
+INSERT INTO primes VALUES ('THERE IS', '&Leps', '1');
+INSERT INTO primes VALUES ('HAVE', '&Leps', '2');
+
+INSERT INTO primes VALUES ('LIVE', '&Ld', '1');
+INSERT INTO primes VALUES ('DIE', '&Ld', '1');
+
+INSERT INTO primes VALUES ('WHEN', '&Time', '1');
+INSERT INTO primes VALUES ('TIME', '&Time', '1');
+INSERT INTO primes VALUES ('NOW', '&Time', '1');
+INSERT INTO primes VALUES ('BEFORE', '&Time', '1');
+INSERT INTO primes VALUES ('AFTER', '&Time', '1');
+INSERT INTO primes VALUES ('A LONG TIME', '&Time', '1');
+INSERT INTO primes VALUES ('A SHORT TIME', '&Time', '1');
+INSERT INTO primes VALUES ('FOR SOME TIME', '&Time', '1');
+INSERT INTO primes VALUES ('MOMENT', '&Time', '1');
+
+INSERT INTO primes VALUES ('WHERE', '&Space', '1');
+INSERT INTO primes VALUES ('PLACE', '&Space', '1');
+INSERT INTO primes VALUES ('HERE', '&Space', '1');
+INSERT INTO primes VALUES ('ABOVE', '&Space', '1');
+INSERT INTO primes VALUES ('BELOW', '&Space', '1');
+INSERT INTO primes VALUES ('FAR', '&Space', '1');
+INSERT INTO primes VALUES ('NEAR', '&Space', '1');
+INSERT INTO primes VALUES ('SIDE', '&Space', '1');
+INSERT INTO primes VALUES ('INSIDE', '&Space', '2');
+
+INSERT INTO primes VALUES ('NOT', '&LogicalConcepts', '2');
+INSERT INTO primes VALUES ('MAYBE', '&LogicalConcepts', '1');
+INSERT INTO primes VALUES ('CAN', '&LogicalConcepts', '1');
+INSERT INTO primes VALUES ('BECAUSE', '&LogicalConcepts', '3');
+INSERT INTO primes VALUES ('IF', '&LogicalConcepts', '2');
+
+INSERT INTO primes VALUES ('VERY', '&Intensifier', '1');
+INSERT INTO primes VALUES ('MORE', '&Intensifier', '1');
+
+INSERT INTO primes VALUES ('LIKE', '&Similarity', '1');
+INSERT INTO primes VALUES ('AS', '&Similarity', '1');
+INSERT INTO primes VALUES ('WAY', '&Similarity', '1');
+-- INSERT INTO primes VALUES ('', '&', '');
