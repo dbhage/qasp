3aa28d8b47769ebae68936a7f172808ab4d9f7a4
Dwijesh Bhageerutty <neerav789@gmail.com>
2014-01-06 20:09:48 -0500

    Set up test suite and test classes for the project.

---
+++ test/memory/MemoryTest.java
@@ -0,0 +1,355 @@
+/*
+ * Copyright (c) 2014, Dwijesh Bhageerutty
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package memory;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Dwijesh Bhageerutty
+ */
+public class MemoryTest {
+    
+    public MemoryTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getConceptNodesForMolecule method, of class Memory.
+     */
+    @Test
+    public void testGetConceptNodesForMolecule() {
+    }
+
+    /**
+     * Test of addDefinitionNode method, of class Memory.
+     */
+    @Test
+    public void testAddDefinitionNode() {
+    }
+
+    /**
+     * Test of addConceptNode method, of class Memory.
+     */
+    @Test
+    public void testAddConceptNode() {
+    }
+
+    /**
+     * Test of addStateNode method, of class Memory.
+     */
+    @Test
+    public void testAddStateNode() {
+    }
+
+    /**
+     * Test of addEventNode method, of class Memory.
+     */
+    @Test
+    public void testAddEventNode() {
+    }
+
+    /**
+     * Test of getDefinitionNodes method, of class Memory.
+     */
+    @Test
+    public void testGetDefinitionNodes() {
+    }
+
+    /**
+     * Test of getCurrentConversationStartTime method, of class Memory.
+     */
+    @Test
+    public void testGetCurrentConversationStartTime() {
+    }
+
+    /**
+     * Test of getNoOfConversations method, of class Memory.
+     */
+    @Test
+    public void testGetNoOfConversations() {
+    }
+
+    /**
+     * Test of getNoOfNodes method, of class Memory.
+     */
+    @Test
+    public void testGetNoOfNodes() {
+    }
+
+    /**
+     * Test of startConversation method, of class Memory.
+     */
+    @Test
+    public void testStartConversation() {
+    }
+
+    /**
+     * Test of endCurrentConversation method, of class Memory.
+     */
+    @Test
+    public void testEndCurrentConversation() {
+    }
+
+    /**
+     * Test of getConcept method, of class Memory.
+     */
+    @Test
+    public void testGetConcept() {
+    }
+
+    /**
+     * Test of getMoleculeNode method, of class Memory.
+     */
+    @Test
+    public void testGetMoleculeNode() {
+    }
+
+    /**
+     * Test of isEmpty method, of class Memory.
+     */
+    @Test
+    public void testIsEmpty() {
+    }
+
+    /**
+     * Test of getNoOfNodesInConversation method, of class Memory.
+     */
+    @Test
+    public void testGetNoOfNodesInConversation() {
+    }
+
+    /**
+     * Test of wordExists method, of class Memory.
+     */
+    @Test
+    public void testWordExists() {
+    }
+
+    /**
+     * Test of getEventNodes method, of class Memory.
+     */
+    @Test
+    public void testGetEventNodes() {
+    }
+
+    /**
+     * Test of setEventNodes method, of class Memory.
+     */
+    @Test
+    public void testSetEventNodes() {
+    }
+
+    /**
+     * Test of getStateNodes method, of class Memory.
+     */
+    @Test
+    public void testGetStateNodes() {
+    }
+
+    /**
+     * Test of setStateNodes method, of class Memory.
+     */
+    @Test
+    public void testSetStateNodes() {
+    }
+
+    /**
+     * Test of getMoleculeNodes method, of class Memory.
+     */
+    @Test
+    public void testGetMoleculeNodes() {
+    }
+
+    /**
+     * Test of setMoleculeNodes method, of class Memory.
+     */
+    @Test
+    public void testSetMoleculeNodes() {
+    }
+
+    /**
+     * Test of getDefNodes method, of class Memory.
+     */
+    @Test
+    public void testGetDefNodes() {
+    }
+
+    /**
+     * Test of setDefNodes method, of class Memory.
+     */
+    @Test
+    public void testSetDefNodes() {
+    }
+
+    /**
+     * Test of getTextNodes method, of class Memory.
+     */
+    @Test
+    public void testGetTextNodes() {
+    }
+
+    /**
+     * Test of getAllConcepts method, of class Memory.
+     */
+    @Test
+    public void testGetAllConcepts() {
+    }
+
+    /**
+     * Test of setAllConcepts method, of class Memory.
+     */
+    @Test
+    public void testSetAllConcepts() {
+    }
+
+    /**
+     * Test of getConversations method, of class Memory.
+     */
+    @Test
+    public void testGetConversations() {
+    }
+
+    /**
+     * Test of setConversations method, of class Memory.
+     */
+    @Test
+    public void testSetConversations() {
+    }
+
+    /**
+     * Test of getMoleculesToConceptMap method, of class Memory.
+     */
+    @Test
+    public void testGetMoleculesToConceptMap() {
+    }
+
+    /**
+     * Test of setMoleculesToConceptMap method, of class Memory.
+     */
+    @Test
+    public void testSetMoleculesToConceptMap() {
+    }
+
+    /**
+     * Test of getConceptNo method, of class Memory.
+     */
+    @Test
+    public void testGetConceptNo() {
+    }
+
+    /**
+     * Test of getCurrentConversation method, of class Memory.
+     */
+    @Test
+    public void testGetCurrentConversation() {
+    }
+
+    /**
+     * Test of getEvent method, of class Memory.
+     */
+    @Test
+    public void testGetEvent() {
+    }
+
+    /**
+     * Test of getWordNodes method, of class Memory.
+     */
+    @Test
+    public void testGetWordNodes_0args() {
+    }
+
+    /**
+     * Test of getWordNodes method, of class Memory.
+     */
+    @Test
+    public void testGetWordNodes_String() {
+    }
+
+    /**
+     * Test of setWordNodes method, of class Memory.
+     */
+    @Test
+    public void testSetWordNodes() {
+    }
+
+    /**
+     * Test of getVerbNodes method, of class Memory.
+     */
+    @Test
+    public void testGetVerbNodes() {
+    }
+
+    /**
+     * Test of setVerbNodes method, of class Memory.
+     */
+    @Test
+    public void testSetVerbNodes() {
+    }
+
+    /**
+     * Test of setTextNodes method, of class Memory.
+     */
+    @Test
+    public void testSetTextNodes() {
+    }
+
+    /**
+     * Test of hasDefinitionNode method, of class Memory.
+     */
+    @Test
+    public void testHasDefinitionNode() {
+    }
+
+    /**
+     * Test of getDefinitionNode method, of class Memory.
+     */
+    @Test
+    public void testGetDefinitionNode() {
+    }
+    
+}
---
+++ test/memory/node/ConceptTypeTest.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2014, Dwijesh Bhageerutty
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package memory.node;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Dwijesh Bhageerutty
+ */
+public class ConceptTypeTest {
+    
+    public ConceptTypeTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of values method, of class ConceptType.
+     */
+    @Test
+    public void testValues() {
+    }
+
+    /**
+     * Test of valueOf method, of class ConceptType.
+     */
+    @Test
+    public void testValueOf() {
+    }
+
+    /**
+     * Test of stringToConceptType method, of class ConceptType.
+     */
+    @Test
+    public void testStringToConceptType() {
+    }
+    
+}
